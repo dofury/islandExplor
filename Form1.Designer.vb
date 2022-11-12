922c5e7b3aad435b15f32926f577a6bd10060da6
dofury <cha_dh1004@naver.com>
2022-11-13 02:37:20 +0900

    섬이름

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,7 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1296, 758)
+        Me.Name = "Form1"
+        Me.Text = "섬"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
